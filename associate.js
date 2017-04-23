7a7f2b1aa023ffb598387f8fef02dab2957e62f9
Matthew Knox <mrkno@users.noreply.github.com>
2017-04-23 14:40:07 +1200

    Add caching, loopback and ES6 to associate (#2)

--- associate.js
+++ associate.js
@@ -1,84 +1,107 @@
-exports.match = function (event, commandPrefix) {
-    if (event.arguments[0] === commandPrefix + 'associate') {
-        return true;
+const ensure = (obj, key, def = {}) => {
+    if (!obj[key]) {
+        obj[key] = def;
     }
-    
-	var s = event.body.toLowerCase();
-	for (var assoc in exports.config[event.thread_id]) {
-        var match;
-
-        //If it isn't a valid regex, terrible stuff happens
-        try {
-            var regex = new RegExp(assoc, 'gim');
-            match = s.match(regex);
-        } catch(err) {
-            match = s.indexOf(assoc.toLowerCase()) !== -1;
-        }
-
-        if (match) {
-            if (!event.__associateCmd) {
-                event.__associateCmd = {
-                    thread: event.thread_id,
-                    responses: []
-                };
-            }
-            event.__associateCmd.responses.push(exports.config[event.thread_id][assoc]);
-		}
-	}
-	return !!event.__associateCmd;
+    return obj[key];
 };
 
-var toggleAssociation = function(thread, hook, text) {
-    hook = hook.toLowerCase();
-    if (!text) {
-        if (exports.config[thread] && exports.config[thread][hook]) {
-            delete exports.config[thread][hook];
+const runCache = {};
+
+const updateCache = () => {
+    for (let thread in exports.config) {
+        if (thread === 'settings') {
+            continue;
         }
+        runCache[thread] = [];
+        for (let assoc in exports.config[thread]) {
+            const res = {
+                association: assoc.toLowerCase(),
+                out: exports.config[thread][assoc]
+            };
+            try {
+                const regex = new RegExp(assoc, 'gim');
+                res.test = regex.test.bind(regex);
+            }
+            catch (e) {
+                res.test = input => input.toLowerCase().contains(res.association);
+            }
+            runCache[thread].push(res);
+        }
+    }
+};
+
+exports.load = updateCache;
+
+exports.match = (event, commandPrefix) => {
+    if (event.arguments[0] === `${commandPrefix}associate`) {
+        return true;
+    }
+
+    if (event.__associateLoopback) {
         return false;
     }
 
-    if (!exports.config[thread]) {
-		exports.config[thread] = {};
+	for (const assoc of runCache[event.thread_id] || []) {
+		if (assoc.test(event.body)) {
+			ensure(event, '__associateResp', []).push(assoc.out);
+		}
 	}
-	exports.config[thread][hook] = text;
-	return true;
-},
+	return !!event.__associateResp;
+}
 
-printAssociations = function(api, event) {
-	var assoc = exports.config[event.thread_id];
-	var message = '';
-	for (var a in assoc) {
-		message += a + ' → ' + assoc[a] + '\n';
-	}
-	message.trim();
+const toggleAssociation = (thread, hook, text) => {
+    hook = hook.toLowerCase();
+    if (text) {
+        ensure(exports.config, thread)[hook] = text;
+    }
+    else {
+        if (ensure(exports.config, thread)[hook]) {
+            delete exports.config[thread][hook];
+        }
+    }
+    updateCache();
+};
+
+const printAssociations = (api, event) => {
+	const assoc = ensure(exports.config, event.thread_id);
+	const message = Object.keys(assoc).map(a => `${a} →\n\t${assoc[a]}`).join('\n');
 	api.sendMessage(message, event.thread_id);
-},
+};
 
-clear = function(api, event) {
+const clear = (api, event) => {
 	exports.config[event.thread_id] = {};
 	api.sendMessage('Associations cleared.', event.thread_id);
 };
 
-exports.run = function (api, event) {
-    var isCommand = event.arguments[0] === api.commandPrefix + 'associate';
-	if (isCommand && event.arguments.length === 1) {
+const executeResponses = (api, event) => {
+    const outSettings = ensure(ensure(exports.config, 'settings'), event.thread_id);
+    for (let response of event.__associateResp) {
+        api.sendMessage(response, event.thread_id);
+        if (outSettings.loopback === true) {
+            const newEvent = shim.createEvent(event.thread_id, event.sender_id, event.sender_name, response);
+            newEvent.event_source = event.event_source;
+            newEvent.__associateLoopback = true;
+            process.nextTick(exports.platform.onMessage.bind(null, api, newEvent));
+        }
+    }
+};
+
+exports.run = (api, event) => {
+	if (event.__associateResp) {
+		return executeResponses(api, event);
+	}
+
+	if (event.arguments.length === 1 || event.arguments.length === 2 && event.arguments[1] === 'print') {
 		return printAssociations(api, event);
 	}
-    
-	if (isCommand && event.arguments[1] === 'clear') {
+
+	if (event.arguments.length === 1 && event.arguments[1] === 'clear') {
 		return clear(api, event);
 	}
-    
-    if (!isCommand && event.__associateCmd) {
-        for (var i = 0; i < event.__associateCmd.responses.length; i++) {
-            api.sendMessage(event.__associateCmd.responses[i], event.__associateCmd.thread);
-        }
-        return;
-    }
-    
-    if (event.arguments.length !== 2 && event.arguments.length !== 3) {
-        return api.sendMessage('WTF are you doing????!', event.thread_id);
-    }
+
+	if (event.arguments.length !== 2 && event.arguments.length !== 3) {
+		return api.sendMessage('WTF are you doing????!', event.thread_id);
+	}
 
 	toggleAssociation(event.thread_id, event.arguments[1], event.arguments[2]);
 	api.sendMessage('Association changed.', event.thread_id);
